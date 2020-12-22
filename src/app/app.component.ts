8fd543512420e0c9eba917b74d6d8622b0f5bda1
chrissyast <cjastles@gmail.com>
2020-12-22 17:04:35 +0000

    min example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//import styles from "../styles.scss";   // "export 'default' (imported as 'styles') was not found in '../styles.scss'
+const styles = require("../styles.scss")
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scss-import-ng-eleven';
+export class AppComponent implements OnInit {
+  buttonColor = styles["colors-primary"]; //"#387ef5"
+
+  ngOnInit() {
+      console.log(styles);
+      console.log(styles["colors-primary"]);
+  }
 }
